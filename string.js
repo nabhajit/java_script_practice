005221b6eae4c9716f01475c1310e97227dba8a0
Coding_variable <roynabhajit@gmail.com>
2024-09-06 14:35:51 +0530

    string

---
+++ string.js
@@ -0,0 +1,42 @@
+const name = "Nabhajit"
+
+const val = 20;
+
+console.log(`Hello my name is ${name} and value is ${val}`);   //Good practice
+
+const again_name = new  String('Nabhajit')
+
+console.log(again_name);
+
+console.log(again_name.toUpperCase());
+
+console.log(again_name[0]);
+
+console.log(again_name.charAt(0));  
+
+console.log(again_name.length);
+
+console.log(again_name.indexOf('j')); 
+
+const namee = new String ('hello world');
+
+console.log(namee.substring(0 , 4));
+
+console.log(namee.slice(-2, 2));
+
+const NewString = "  hello    ";
+
+console.log(NewString.trim());
+
+const url = "https://www.google.com";
+
+console.log(url.startsWith("https"));
+
+console.log(url.endsWith(".com"));
+
+console.log(url.includes("google"));
+
+console.log(url.split("."));
+
+console.log(url.split(".")[0]);
+
